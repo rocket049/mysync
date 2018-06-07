9cce6a20ae1d7ffcfe5a06fd03a8668862bfea3b
rocket049 <fuhuizn@163.com>
2018-06-07 18:39:47 +0800

    genkey

---
+++ src/genkey/genrsa.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"mysyncd/mycrypto"
+	"flag"
+)
+
+func main() {
+	var k = flag.String("k", "mykey", "[-k name]:generate rsa key pair ,save in name.pub and name.key")
+	flag.Parse()
+	//generate key pair
+	name1 := *k
+	if len(name1) == 0 {
+		panic("no name. please use: -h")
+	}
+	err := mycrypto.GenKeyPair(name1)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
